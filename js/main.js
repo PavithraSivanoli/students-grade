8f98de7b751fad78ff5137f616b6e44382a6f750
unknown <pavithrasivanoli@gmail.com>
2022-12-17 10:48:00 +0530

    updated the files

---
+++ js/main.js
@@ -0,0 +1,35 @@
+var mark = parseInt(prompt("Enter first number: "));
+
+// check the condition
+if(mark >=95 & mark<=100) 
+{
+alert("print o grade ");
+}
+if(mark >=90 & mark<=95) 
+{
+alert("print A grade ");
+}
+if(mark >=80 & mark<=91) 
+{
+alert("print B grade ");
+}
+if(mark >=70 & mark<=81) 
+{
+alert("print C grade ");
+}
+if(mark >=60 & mark<=71)
+{
+alert("print D grade ");
+}
+if(mark >=49 & mark<=61) 
+{
+alert("print E grade ");
+}
+if(mark >=0 & mark<=50) 
+{
+alert("print F grade ");
+}
+else
+{ 
+alert("put invalid input");
+}
